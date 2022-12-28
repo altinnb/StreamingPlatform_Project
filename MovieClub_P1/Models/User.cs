791b3cd8dc18e040987a03b4544b26cef3add329
altinnb <ab52936@ubt-uni.net>
2022-12-28 23:37:41 +0100

    users-and-roles

--- MovieClub_P1/Models/User.cs
+++ MovieClub_P1/Models/User.cs
@@ -2,10 +2,8 @@
 {
     public class User
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Lastname { get; set; }
-        public string username { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
     }
